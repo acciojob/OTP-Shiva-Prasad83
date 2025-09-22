86657ce0915d0f80243eea0b3ac02f91d4655bc2
Shiva-Prasad83 <shivaprasad.ch83@gmail.com>
2025-09-22 15:17:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,28 +1,19 @@
 //your JS code here. If required.
 const inputs=document.querySelectorAll(".code");
-        // console.log(inputs);
 inputs[0].focus();
+        // console.log(inputs);
         inputs.forEach((input,index)=>{
-            input.addEventListener("input",(e)=>{
-                if(index<inputs.length-1){
+            input.addEventListener("keyup",(e)=>{
+                if(index!=inputs.length-1 && e.key!="Backspace"){
                     let focused=inputs[index+1];
 					focused.focus();
                 }
-            });
-			input.addEventListener("keyup",(e)=>{
-				if(e.key=="Backspace" && index!=0){
-					inputs[index].innerText="";
-					let focused=inputs[index-1];
-					focused.focus();
-				}else{
-					inputs[index].innerText="";
-				}
-			})
-			    //            if(index!=0 && e.key=="Backspace"){
-     //                inputs[index].innerText="";
-     //               let focused=inputs[index-1];
-					// focused.focus()
-     //            }else{
-     //                inputs[index].innerText="";
-     //            }
+                if(index!=0 && e.key=="Backspace"){
+                    inputs[index].innerText="";
+                   let focused=inputs[index-1];
+					focused.focus()
+                }else{
+                    inputs[index].innerText="";
+                }
+            })
         })
